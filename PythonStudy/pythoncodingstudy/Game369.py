a7b9089a56350e8c10089db5f644e0f9927af658
yn-j-98 <yena092582@gmail.com>
2024-11-23 19:11:50 +0900

    1

--- PythonStudy/pythoncodingstudy/Game369.py
+++ PythonStudy/pythoncodingstudy/Game369.py
@@ -11,6 +11,6 @@ def solution(order):
         digit = order % 10  # order의 마지막 자릿수
         if digit == 3 or digit == 6 or digit == 9:
             answer += 1  # 3, 6, 9가 있으면 박수 횟수 증가
-        order //= 10  # 마지막 자릿수를 제거
+        order //= 10  # 마지막 자릿수를 제거 
         
     return answer
