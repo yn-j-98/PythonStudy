ce74e2f2bb5fb5c151088fc3ad4cdc63ba640181
yn-j-98 <yena092582@gmail.com>
2024-11-24 22:58:48 +0900

    파이썬 연습

--- PythonStudy/pythoncodingstudy/Lesson120583.py
+++ PythonStudy/pythoncodingstudy/Lesson120583.py
@@ -3,3 +3,11 @@ Created on 2024. 11. 24.
 
 @author: yenaj
 '''
+
+def solution(array, n):
+    answer = 0
+    
+    for i in array:
+        if array ==n:
+            answer += i
+    return answer
