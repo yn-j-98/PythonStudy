805a13b7fd09dd161f732152e04230ef5c7f6a48
yn-j-98 <yena092582@gmail.com>
2024-11-24 23:02:01 +0900

    241124 파이썬 연습

--- PythonStudy/pythoncodingstudy/Lesson120583.py
+++ PythonStudy/pythoncodingstudy/Lesson120583.py
@@ -5,10 +5,5 @@ Created on 2024. 11. 24.
 '''
 
 def solution(array, n):
-    answer = 0
-    
-    for i in array:
-        if i == n:
-            answer += i
-            
-    return answer
+    # array에서 n이 등장하는 횟수를 반환
+    return array.count(n)
